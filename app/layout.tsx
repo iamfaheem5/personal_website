aca4012853b8eba7306ddc7f5c1b2bcb0008ca01
darkstudy45 <114846213+darkstudy45@users.noreply.github.com>
2025-09-22 12:22:22 +0600

    title fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,8 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Google map optimization ',
+  description: 'Expert service for fixing you cosstly mistakes. ',
 }
 
 export default function RootLayout({
